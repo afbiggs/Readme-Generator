867adac46fed3d265ebc8294cf4acfc746a4842b
afbiggs <112245251+afbiggs@users.noreply.github.com>
2022-11-21 01:58:01 -0500

    deleted package.json and all dependencies and reinstalled

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const questions = require("./data/questions");
+const questions = require("./questions");
 // TODO: Create an array of questions for user input
 
 inquirer.prompt(questions).then((response) => {
